bae5ebe4bda62b462f425559202c5386ed2a7080
NhatAnh <nhatanh81096@yahoo.com>
2021-02-11 17:22:16 -0500

    Add FedAvg CNN model

--- src/mnist_model.py
+++ src/mnist_model.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn.functional as F
 
 from torch import nn
+from torchsummary import summary
 
 
 class Model(nn.Module):
@@ -25,3 +26,26 @@ class Model(nn.Module):
 		x = self.fc(self.flatten(x))
 
 		return x
+
+class CNNModel(nn.Module):
+	'''
+	Follow FedAvg CNN Model
+	A CNN with two 5x5 convolution layers (the first with 32 channels, the second with 64, each followed with 2x2 max pooling)
+	A fully connected layer with 512 units and ReLu activation, and a final softmax output layer (1,663,370 total parameters)
+	'''
+
+	def __init__(self):
+		super().__init__()
+
+		self.conv1 = nn.Conv2d(1, 32, kernel_size=5, stride=1, padding=2)
+		self.conv2 = nn.Conv2d(32, 64, kernel_size=5, stride=1, padding=2)
+		self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
+		self.flatten = nn.Flatten()
+		self.fc1 = nn.Linear(3136,512)
+		self.fc2 = nn.Linear(512, 10)
+
+	def forward(self, x):
+		x = self.maxpool(F.relu(self.conv1(x)))
+		x = self.maxpool(F.relu(self.conv2(x)))
+		x = self.fc1(self.flatten(x))
+		x = F.softmax(self.fc2(x), dim=1)
